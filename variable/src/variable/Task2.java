8940eb6ef9812caa4a7a1a78962cd3406b5c53ae
oosima-coder <sneak.liger.zero@gmail.com>
2020-03-20 10:53:57 +0900

    課題提出 - 変数・定数

---
+++ variable/src/variable/Task2.java
@@ -0,0 +1,14 @@
+package variable;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		String name="大島　てつめい";
+		final String TELPHONE ="091-99-799";
+			System.out.println(name+"です");
+			System.out.println("私の電話番号は"+"\'"+TELPHONE+"\'"+"です");
+
+	}
+
+}
