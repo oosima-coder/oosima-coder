5571a0bf58e0323d00e83224318ddbadefb1a8b9
oosima-coder <sneak.liger.zero@gmail.com>
2020-04-01 12:44:38 +0900

    課題提出 - 配列

---
+++ array/src/array/Task1.java
@@ -0,0 +1,26 @@
+package array;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		
+int[] array =  {4, 9, 19, 5, 8, 21, 42, 64, 73, 18, 2};
+		System.out.print("配列:[");
+for(int i = 0; i < array.length; i++) {
+	if(i == array.length-1) {
+		System.out.print(array[i]);
+	}else {
+		System.out.print(array[i]+",");
+	}
+}		System.out.println("]");
+
+int sum = 0;
+for (int num1 : array) {
+	
+	sum += num1 ;
+}
+		System.out.println("合計:"+sum);
+	
+	
+	}
+}
