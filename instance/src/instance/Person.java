5abd07934affb7ca9bc91b25413ced5f19f20a72
oosima-coder <sneak.liger.zero@gmail.com>
2020-04-24 10:12:23 +0900

    再提出 - インスタンス＆クラス

--- instance/src/instance/Person.java
+++ instance/src/instance/Person.java
@@ -2,8 +2,8 @@ package instance;
 
 public class Person {
 
-	 String name = "oosima";
-	 int age = 30;
+	 String name = "";
+	 int age = 0;
 	 public void myProfile() {
 			 System.out.println("私の名前は" + this.name + "です。");
 			 System.out.println("私の年齢は" + this.age + "です。");
