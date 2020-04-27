1afbfbe938486c397c9ccfe64657c623df6c7c58
oosima-coder <sneak.liger.zero@gmail.com>
2020-04-27 10:38:32 +0900

    再提出 -変数・定数

--- variable/src/variable/Tasak1.java
+++ variable/src/variable/Tasak1.java
@@ -4,8 +4,8 @@ public class Tasak1 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println((3+2)*6+5);
-		System.out.println(24%3);
+		System.out.println((3 + 2) * 6 + 5);
+		System.out.println(24 % 3);
 	}
 
 }
--- variable/src/variable/Task1.java
+++ variable/src/variable/Task1.java
@@ -3,10 +3,10 @@ package variable;
 public class Task1 {
 public static void main(String args[]) {
 	double num=31.2;
-	String learning="Java研修";
-	int number=9;
-	long big=2147483648L;
-	char single= '才';
+	String learning = "Java研修";
+	int number = 9;
+	long big = 2147483648L;
+	char single = '才';
 	System.out.println(num);
 	System.out.println(learning);
 	System.out.println(number);
