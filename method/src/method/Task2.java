ce5cd354f1bc9fb814e3ce3c266ef68a18d9b9b8
oosima-coder <sneak.liger.zero@gmail.com>
2020-04-27 11:40:15 +0900

    再提出 - メソッド

--- method/src/method/Task2.java
+++ method/src/method/Task2.java
@@ -1,3 +1,4 @@
+
 package method;
 
 public class Task2 {
@@ -5,11 +6,11 @@ public class Task2 {
 	public static void main(String[] args) {
 		double test = getTriangleArea(2, 2);
 		System.out.println("三角形の面積");
-		System.out.println(Math.round(test));
+		System.out.println(test);
 
 	}
 	public static double getTriangleArea(double num1, double high) {
-		return num1 * high / 2;
+		return Math.round(num1 * high / 2);
 	}
 
 }
