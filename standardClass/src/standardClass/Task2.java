e4fc1eec8dfcd4794fd7089bafb4a9a60c8d00af
oosima-coder <sneak.liger.zero@gmail.com>
2020-05-04 11:38:52 +0900

    再提出 - 標準クラス

--- standardClass/src/standardClass/Task2.java
+++ standardClass/src/standardClass/Task2.java
@@ -3,12 +3,12 @@ package standardClass;
 public class Task2 {
 
 	public static void main(String[] args) {
-	String text1 = "Java Programming Training";
-	String text2 = text1.substring(17);
-	String text3 = text1.substring(0, 16);
+		String text1 = "Java Programming Training";
+		String text2 = text1.substring(17);
+		String text3 = text1.substring(0, 16);
 
-	System.out.println(text2);
-	System.out.println(text3);
+		System.out.println(text2);
+		System.out.println(text3);
 	}
 
 }
--- standardClass/src/standardClass/Task7.java
+++ standardClass/src/standardClass/Task7.java
@@ -5,29 +5,30 @@ import java.util.Date;
 public class Task7 {
 	public static void main(String[] args) {
 		Calendar cal2 = Calendar.getInstance();
+		Calendar cal3 = Calendar.getInstance();
 		Date date = new Date();
-		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy年MM月dd日");
-		SimpleDateFormat sdf2 = new SimpleDateFormat("yyyy年MM月dd日");
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
 
 		cal2.setTime(date);
+		cal3.setTime(date);
+
 		cal2.add(Calendar.DAY_OF_MONTH, 7);
 		Date afterWeek = cal2.getTime();
-		
-		cal2.add(Calendar.YEAR, 1);
-		Date oneday = cal2.getTime();
-		
+
+		cal3.add(Calendar.YEAR, 1);
+		Date oneday = cal3.getTime();
+
+		SimpleDateFormat sdf1 = new SimpleDateFormat("yyyy年MM月dd日");
 		//現在日時を表示
 		System.out.println("現在日時: " + sdf1.format(date));
 		//一週間後を表示
-		System.out.println("１週間後: " + sdf2.format(afterWeek));
+		System.out.println("１週間後: " + sdf1.format(afterWeek));
 		//一年後を表示
-		System.out.print("１年後  : " + sdf.format(oneday));
+		System.out.print("１年後  : " + sdf1.format(oneday));
 		//曜日
 		String [] weekDay = {"日", "月", "火", "水", "木", "金", "土"};
-		int week = cal2.get(Calendar.DAY_OF_WEEK);
+		int week = cal3.get(Calendar.DAY_OF_WEEK);
 		System.out.println("(" + weekDay[week - 1] + ")");
-		
+
 		//System.out.println(cal2.get(Calendar.DAY_OF_WEEK) );
 	}
 }
