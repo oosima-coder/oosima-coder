f6eb234311c0ebc9f6ae02795cd1c640eeb95e17
oosima-coder <sneak.liger.zero@gmail.com>
2020-05-07 13:13:06 +0900

    再提出 - 配列

--- array/src/array/Task2.java
+++ array/src/array/Task2.java
@@ -1,20 +1,22 @@
 package array;
 
 public class Task2 {
-public static void main(String[] args) {
-int[] array =  {7, 6, 5, 4, 3, 2, 1};
-for(int i = 0; i < array.length; i++) {
-	if(i == array.length-1) {
-		System.out.println(array[i]);
-	}else {
-		System.out.print(array[i] + ",");
-	}
-}		
+	public static void main(String[] args) {
+		int[] array =  {7, 6, 5, 4, 3, 2, 1};
+		for(int i = 0; i < array.length; i++) {
+			if(i == array.length-1) {
+				System.out.println(array[i]);
+			}else {
+				System.out.print(array[i] + ",");
+			}
+		}		
 
-for (int i = array.length-1; i >= 0; i--) {
-	if(i == 0) {
-		System.out.println(array[i]);
-	}else {
-		System.out.print(array[i] + "," );
+		for (int i = array.length-1; i >= 0; i--) {
+			if(i == 0) {
+				System.out.println(array[i]);
+			}else {
+				System.out.print(array[i] + "," );
+			}
+		}
 	}
-}}}
+}
--- array/src/array/Task3.java
+++ array/src/array/Task3.java
@@ -4,26 +4,27 @@ import java.util.Random;
 
 public class Task3 {
 
-public static void main(String[] args) {
-	int[] nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-			System.out.print("[");
-	Random r = new Random();
+	public static void main(String[] args) {
+		int[] nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		System.out.print("[");
+		Random r = new Random();
 		int max = 0;
 		int min = 100;
 
-	for(int i = 0 ; i < 6 ; i++) {
-		int num = nums[r.nextInt(10)];
+		for(int i = 0 ; i < 6 ; i++) {
+			int num = nums[r.nextInt(10)];
 
-		if(i == 5) {
-			System.out.print(num);
-		}else {
-			System.out.print(num + ",");}
-		
-		max = Math.max(max, num);
-		min = Math.min(min, num);
-	}		System.out.println("]");
-	
-			System.out.println("最大値：" + max);
-			System.out.println("最小値：" + min);
+			if(i == 5) {
+				System.out.print(num);
+			}else {
+				System.out.print(num + ",");}
 
-	}}
+			max = Math.max(max, num);
+			min = Math.min(min, num);
+		}		System.out.println("]");
+
+		System.out.println("最大値：" + max);
+		System.out.println("最小値：" + min);
+
+	}
+}
